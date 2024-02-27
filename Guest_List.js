a50c0730373da36600ce563d3ea532df5af9a5ba
Abdul wasay khan <156195187+abdulwasayk14@users.noreply.github.com>
2024-02-27 09:32:07 +0500

    Add files via upload

---
+++ Guest_List.js
@@ -0,0 +1,5 @@
+"use strict";
+let Guest_List = ['Sarfaraz Ahmed', 'Babar Azam', 'shoaib Malik'];
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('Respected Sir. ' + Guest_List[i] + ',\nIt is our pleasure to invite you,Thank You\n\nThank You)!');
+}
---
+++ Guest_List.ts
@@ -0,0 +1,10 @@
+
+
+let Guest_List :string[] = ['Sarfaraz Ahmed','Babar Azam','shoaib Malik'];
+
+for(let i=0; i<Guest_List.length; i++){
+
+    console.log('Respected Sir. ' + Guest_List[i] + ',\nIt is our pleasure to invite you,Thank You\n\nThank You)!')
+
+}
+    
